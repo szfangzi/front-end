d1a51f614cb5319ee0783bc5836fe57879ce3a1d
jason <szfangziwei@qq.com>
2016-04-12 16:35:17 +0800

    sfzl

---
+++ sfzl/public/shop/detail.js
@@ -0,0 +1,15 @@
+$(function(){
+	$(".slideBox").slide({
+		mainCell:".bd ul",
+		autoPlay:true,
+		trigger:"click",
+		effect:"fold"
+	});
+	$('.all_type').hover(function(){
+		$('.menu_main').show();
+	});
+	$('.all_typebox').mouseleave(function(){
+		$('.menu_main').hide();
+	});
+	
+});
